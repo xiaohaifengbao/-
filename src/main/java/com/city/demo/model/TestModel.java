31fc1724961819427c08d69d337ff44aa5608733
yuminghao <xiaohaifengbao@126.com>
2019-08-18 02:06:22 +0800

    测试提交V1.0

---
+++ src/main/java/com/city/demo/model/TestModel.java
@@ -0,0 +1,12 @@
+package com.city.demo.model;
+
+/**
+ * @author yuminghao
+ * @Date 2019/8/18 2:03
+ * @About 测试V1.0
+ */
+public class TestModel {
+    public static void main(String[] args) {
+        System.out.println("这是-----------master");
+    }
+}
